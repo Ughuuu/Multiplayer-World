bc50eaf7063139eda28b2fddaf3e9b0cfaec2c93
Dragos Daian <daian.dragos@yahoo.com>
2023-12-25 14:23:41 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,11 @@ const server = Bun.serve<WebSocketData>({
     if (filePath == "/") {
       filePath = "index.html";
     }
-    let response = new Response(Bun.file(publicFolder + filePath))
-    response.headers.set("Cross-Origin-Opener-Policy", "same-origin")
-    response.headers.set("Cross-Origin-Embedder-Policy", "require-corp")
-    return response
+    //let response = new Response(Bun.file(publicFolder + filePath))
+    //response.headers.set("Cross-Origin-Opener-Policy", "same-origin")
+    //response.headers.set("Cross-Origin-Embedder-Policy", "require-corp")
+    //return response
+    return new Response(publicFolder + filePath)
   },
   websocket: {
     async open(ws: ServerWebSocket<WebSocketData>) {
